c7316fd8953682c12f880c02eb2671578f956ccc
Chris Morrell <chris@cmorrell.com>
2018-10-31 15:24:08 -0400

    Additional JS bundles

--- rollup.config.js
+++ rollup.config.js
@@ -2,19 +2,38 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 
-export default {
-	input: 'js/src/main.js',
-	output: {
-		file: 'js/dist/aire.js',
-		format: 'iife'
-	},
-	plugins: [
-		resolve(),
-		commonjs({
-			include: 'node_modules/**',
-		}),
-		babel({
-			exclude: 'node_modules/**'
-		})
-	]
-};
+const plugins = [
+	resolve(),
+	commonjs({
+		include: 'node_modules/**',
+	}),
+	babel({
+		exclude: 'node_modules/**'
+	})
+];
+
+export default [
+	{
+		input: 'js/src/main.js',
+		output: {
+			file: 'js/dist/aire.js',
+			format: 'iife'
+		},
+		plugins
+	}, {
+		input: 'js/src/Aire.js',
+		output: {
+			file: 'js/dist/aire-umd.js',
+			name: 'Aire',
+			format: 'umd'
+		},
+		plugins
+	}, {
+		input: 'js/src/Aire.js',
+		output: {
+			file: 'js/dist/aire.mjs',
+			format: 'esm'
+		},
+		plugins
+	}
+];
