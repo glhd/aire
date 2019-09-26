da63cd7630ef81c27dfa6ca9539a5dd030d975cf
Martin Betz <martinmichaelbetz@gmail.com>
2019-09-26 14:08:36 +0200

    Add: German translation

---
+++ translations/de/common.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'summary' => '{1} Es gibt einen Fehler. Korrigieren Sie ihn, um fortzufahren.|[2,*] Es gibt :count Fehler in diesem Formular. Korrigieren Sie diese, um fortzufahren.',
+];
