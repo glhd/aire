fc6bd77428872e9a2d54826ff555d2b38ae479e2
Benny Rahmat <akunbeben@gmail.com>
2021-07-24 10:42:35 +0700

    chore: added bahasa indonesia translation

---
+++ translations/id/common.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+	'rangkuman' => '{1} Ada kesalahan di halaman ini yang harus anda perbaiki sebelum melanjutkan.|[2,*] Ada :count kesalahan di halaman ini yang harus anda perbaiki sebelum melanjutkan.',
+];
