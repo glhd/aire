054766a12a5836621f42c318b9fdd88972bbe778
Cheyne Rood <cheyne.rood@gmail.com>
2024-08-13 09:02:06 -0600

    errorBag() form option (#134)
    * errorBag() method/functionality
    
    * use getBag
    
    * Add a test
    
    ---------
    
    Co-authored-by: Chris Morrell <inxilpro@users.noreply.github.com>

--- tests/Feature/ServerValidationTest.php
+++ tests/Feature/ServerValidationTest.php
@@ -42,4 +42,33 @@ class ServerValidationTest extends TestCase
 		$this->assertSelectorExists($html, '[data-aire-component="errors"]');
 		$this->assertSelectorContainsText($html, '[data-aire-component="errors"]', 'The generic input field is required');
 	}
+	
+	public function test_it_respects_error_bags()
+	{
+		Route::get('/aire', function() {
+			return view('basic-form');
+		})->middleware('web');
+		
+		Route::post('/default-bag', function(Request $request) {
+			$request->validate([
+				'generic_input' => 'required',
+			]);
+		})->middleware('web');
+		
+		Route::post('/bag2', function(Request $request) {
+			$request->validateWithBag('bag2', [
+				'generic_input' => 'required',
+			]);
+		})->middleware('web');
+		
+		$this->post('/default-bag')->assertRedirect();
+		$html = $this->get('/aire')->getContent();
+
+		$this->assertSelectorClassNames($html, '#generic_input_group', 'is-invalid');
+		
+		$this->post('/bag2')->assertRedirect();
+		$html = $this->get('/aire')->getContent();
+		
+		$this->assertSelectorMissingClassNames($html, '#generic_input_group', 'is-invalid');
+	}
 }
